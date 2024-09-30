9f71b20dd10b2cea635e44f8d220a39c7a887579
Chia Liu <rrliu@ad.unc.edu>
2024-09-30 14:33:12 -0400

    Practice importing functions + syntax!

---
+++ lessons/importing.py
@@ -0,0 +1,11 @@
+"""Practice importing a specific function or variable"""
+
+from lessons.scope_practice import remove_chars
+
+__author__: str = "730763577"
+
+
+# syntax for importing:
+# from <package name>.<module name> import <function or variable name>
+
+remove_chars
