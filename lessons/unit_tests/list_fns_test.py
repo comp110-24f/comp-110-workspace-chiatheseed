0d877545c8f51482d1ca6622e5895e84ee5a3635
Chia Liu <rrliu@ad.unc.edu>
2024-10-16 17:30:18 -0400

    Testing functions with pytest!

---
+++ lessons/unit_tests/list_fns_test.py
@@ -0,0 +1,22 @@
+"""Doing stuff w python libraries! (I think)"""
+
+from lessons.unit_tests.list_fns import get_first, remove_first, get_and_remove_first
+
+
+def test_get_first() -> None:
+    """Test that get first returns first elem"""
+    fruits: list[str] = ["apples", "oranges", "bananas"]
+    assert get_first(fruits) == "apples"
+
+def test_remove_first_ret_value() -> None:
+    """Test that remove first returns None"""
+    fruits: list[str] = ["apples", "oranges", "bananas"]
+    assert remove_first(fruits) == None
+
+def test_remove_first_behavior() -> None:
+    """Test that remove first removes first element"""
+    fruits: list[str] = ["apples", "oranges", "bananas"]
+    remove_first(fruits)
+    assert fruits == ["oranges", "bananas"]
+
+def test_get_first_edge
