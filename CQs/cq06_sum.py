c8618fc24bdaf56bd8bbff8a37ddc23e1dfd4150
Chia Liu <rrliu@ad.unc.edu>
2024-10-16 23:22:43 -0400

    Made comments

--- CQs/cq06_sum.py
+++ CQs/cq06_sum.py
@@ -13,10 +13,12 @@ def w_sum(vals: list[float]) -> float:
     index: int = 0
     sumelem: float = vals[index]
     index += 1
+    # index as separate variable will track index for while loop
 
     while index < len(vals):
         sumelem += vals[index]
         index += 1
+        # prevents while loop from running infinitely
     return sumelem
 
 
@@ -25,6 +27,7 @@ def f_sum(vals: list[float]) -> float:
 
     if len(vals) == 0:
         return 0.0
+    # if list is empty, returns 0.0
 
     sumelem: float = 0.0
 
@@ -39,10 +42,14 @@ def f_range_sum(vals: list[float]) -> float:
 
     if len(vals) == 0:
         return 0.0
+    # if list is empty, returns 0.0
 
     sumelem: float = 0.0
 
     for floats in range(0, len(vals)):
         # for in loops give more specificity to range!
-        sumelem += floats
+        sumelem += vals[floats]
+        # Note! it is += vals[floats] because floats is the INDEX
+        # if you just did += floats, it would always add up to 3.0 because range counts the index
+
     return sumelem
