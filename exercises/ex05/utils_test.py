1581ffd3ea38ee02bf8cd8be5e4405444cab540d
Chia Liu <rrliu@ad.unc.edu>
2024-10-22 15:04:08 -0400

    working on unit tests for EX05

---
+++ exercises/ex05/utils_test.py
@@ -0,0 +1,23 @@
+"""defining unit tests for utils.py"""
+
+__author__ = "730763577"
+
+# 3 unit test functions for each function!!
+    # At least 1 edge case
+    # At least 2 use cases: one that tests what the function should return and one that tests how it should mutate (or not mutate) its input.
+
+def test_only_evens_() -> None:
+
+
+
+def test_only_evens() -> None:
+
+
+def test_only_evens() -> None:
+
+
+
+def test_sub() -> None:
+
+
+def test_add_at_index() -> None:
