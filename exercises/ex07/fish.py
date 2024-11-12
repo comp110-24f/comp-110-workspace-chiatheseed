3b4c0a54c7ee801138aa514e44a31c221ee3f129
Chia Liu <rrliu@ad.unc.edu>
2024-11-11 21:16:22 -0500

    fish

--- exercises/ex07/fish.py
+++ exercises/ex07/fish.py
@@ -1,13 +1,17 @@
 """File to define Fish class."""
 
+__author__: str = "730763577"
+
 
 class Fish:
     age: int
 
     def __init__(self):
+        """Creates new Fish object"""
         self.age = 0
         return None
 
     def one_day(self):
+        """Increases age of each fish by 1 each day"""
         self.age += 1
         return None
