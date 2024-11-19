1ed955bce71cd561f0c3fbf3b26330e6e87c49a3
Chia Liu <rrliu@ad.unc.edu>
2024-11-19 08:52:46 -0500

    Linked List lecture practice nov 13

---
+++ lessons/linked_list.py
@@ -0,0 +1,50 @@
+from __future__ import annotations
+
+
+class Node:
+    value: int
+    next: Node | None
+
+    def __init__(self, value: int, next: Node | None):
+        self.value = value
+        self.next = next
+
+
+two: Node = Node(2, None)
+one: Node = Node(1, two)
+courses: Node = Node(110, Node(210, Node(301, None)))
+
+
+def to_str(head: Node | None) -> str:
+    """Represent a Linked List as a str."""
+    if head is None:
+        return "None"
+    else:
+        rest: str = to_str(head.next)
+        return f"{head.value} -> {rest}"
+
+
+print(to_str(one))
+print(to_str(courses))
+
+
+def last(head: Node) -> int:
+    """Return the last value of a non-empty list."""
+    # Base Case: when head is the last node
+    # return it's value!
+    print(f"Enter last({head.value})")
+    if head.next is None:
+        print(f"Return base case: {head.value}")
+        return head.value
+    # Recursive Case:
+    # figure out the last node (recursive call)
+    # in the "rest of the list"
+    # return this value!
+    else:
+        rest: int = last(head.next)
+        print(f"Return recur: {head.value} -> {rest}")
+        return rest
+
+
+print(last(one))  # expect to print 2
+print(last(courses))  # expect to print 301
