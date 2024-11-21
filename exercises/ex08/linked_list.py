28c441c90e5ca5e8584719676b4361234a1444f7
Chia Liu <rrliu@ad.unc.edu>
2024-11-20 21:05:36 -0500

    submitted to autograder, but need to fix some annotations

--- exercises/ex08/linked_list.py
+++ exercises/ex08/linked_list.py
@@ -16,11 +16,21 @@ class Node:
         self.value = value
         self.next = next
 
+    def __str__(self) -> str:
+        """Produce a string representation of a linked list."""
+        rest: str
+        if self is None:
+            return "None"
+        else:
+            rest = str(self.next)
+            return f"{self.value} -> {rest}"
 
+
+"""
 three: Node = Node(30, None)
 two: Node = Node(20, three)
 one: Node = Node(10, two)
-courses: Node = Node(110, Node(210, Node(301, None)))
+courses: Node = Node(110, Node(210, Node(301, None)))"""
 
 
 def to_str(head: Node | None) -> str:
@@ -28,6 +38,7 @@ def to_str(head: Node | None) -> str:
     if head is None:
         return "None"
     else:
+        # creates a variable that holds the str linked list up until the current node
         rest: str = to_str(head.next)
         return f"{head.value} -> {rest}"
 
@@ -45,9 +56,7 @@ def last(head: Node) -> int:
         print(f"Return base case: {head.value}")
         return head.value
     # Recursive Case:
-    # figure out the last node (recursive call)
     # in the "rest of the list"
-    # return this value!
     else:
         rest: int = last(head.next)
         print(f"Return recur: {head.value} -> {rest}")
@@ -59,7 +68,7 @@ print(last(courses))  # expect to print 301"""
 
 
 def value_at(head: Node | None, index: int) -> int:
-    """Returns the value of the Node stored at a given index or raises IndexError."""
+    """Returns the value of the Node stored at a given index."""
     # edge case - empty list
     if head is None:
         raise IndexError("Index is out of bounds on the list.")
@@ -68,9 +77,54 @@ def value_at(head: Node | None, index: int) -> int:
         return head.value
     return value_at(head.next, index - 1)
 
-# VALUE AT
-# inputs: head (Node | None) and index (int)
 
-def max(head: Node)
+def max(head: Node | None) -> int:
     """Returns max value in the linked list."""
-    
+    # edge case - if the linked list is empty
+    if head is None:
+        raise ValueError("Cannot call max with None")
+    # if there is only one node in the linked list
+    if head.next is None:
+        return head.value
+    # if current node's value is greater, returns head.value
+    # MISTAKE: comparing max(head.value) with max(head.next)
+    # don't call max(head.value) since head.value is the current node evaluated in the current funct frame
+    elif head.value > max(head.next):
+        return head.value
+    # otherwise, returns MAX(head.next)
+    else:
+        return max(head.next)
+
+
+def linkify(list: list[int]) -> Node | None:
+    """Returns a Linked List of Nodes with same values, in same order, as input list."""
+    # base case - what the last 'step' is
+    # the spliced list is empty
+    if len(list) == 0:
+        return None
+    # recursive case - create nodes
+    # smth must happen for recursive case to reach base case
+    else:
+        # putting linkify() in the node.next spot links previous node to the next node
+        node = Node(list[0], linkify(list[1:]))
+        # MISTAKE: not returning smth
+        # not returning node leads to IndexError
+        return node
+
+
+# print(linkify([10, 20, 30]))
+
+
+def scale(head: Node | None, factor: int) -> Node | None:
+    """Returns a new linked list of nodes where each value in the input linked list is scaled by factor."""
+    # base case - if list is empty; last step
+    if head is None:
+        return None
+    # if there is just one node in the linked list
+    else:
+        node = Node(head.value * factor, scale(head.next, factor))
+        return node
+
+
+"""print(scale(linkify([1, 2, 3]), 2))
+"""
